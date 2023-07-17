38e14158a6d65b9c484f1085e4d04cd5674e0b11
bastian <bastiangarcesga@gmail.com>
2023-07-16 22:34:07 -0400

    ADD: hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,78 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useForm, Controller } from 'react-hook-form';
 import './App.css'
 import PrimaryKeyInput, { Person } from './PrimaryKeyInput'
 
-// interface IPersonaInfo {
-//   primaryKey: string;
-// }
+interface IPersonaInfo {
+  name: string | undefined;
+  status: string
+}
+
+
 
 function App() {
   const [info, setInfo] = useState<Person[] | null>(null)
+  const { control, handleSubmit, setValue, register, reset } = useForm<IPersonaInfo>();
+
+
+  const onSubmit = (data: IPersonaInfo) => {
+    // Procesar el envío del formulario si es necesario
+    console.log(data);
+    reset()
+  };
+
+  useEffect(() => {
+    if (info && info.length > 0) {
+      // Si hay datos encontrados, establecerlos en los campos del formulario
+      setValue('name', info[0]?.name ?? '');
+      setValue('status', info[0]?.status ?? '');
+    } else {
+      reset()
+    }
+  }, [info, setValue, reset]);
+
+
 
   console.log('info de app', info)
   return (
     <>
       <h1>Formulario</h1>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <PrimaryKeyInput url={'/api/personas/'} setInfo={setInfo} />
-        <input type="text" placeholder="nombre" value={info ? info[0]?.name ?? '' : ''} />
-        <input type="text" placeholder="status" value={info ? info[0]?.status ?? '' : ''} />
+
+        <Controller
+          name='name'
+          control={control}
+          defaultValue={info ? info[0]?.name ?? name : ''}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <input
+              {...field}
+              type="text"
+              placeholder="nombre"
+              // value={info ? info[0]?.name ?? '' : ''}
+              {...register('name', { required: true })}
+              onChange={(e) => setValue('name', e.target.value)}
+            />
+          )}
+        />
+        <Controller
+          name="status"
+          control={control}
+          defaultValue={info ? info[0]?.status ?? '' : ''}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <input
+              {...field}
+              type="text"
+              placeholder="status"
+              // value={info ? info[0]?.status ?? '' : ''}
+              // {...register('status', { required: true })}
+              onChange={(e) => setValue('status', e.target.value)}
+            />
+          )}
+        />
+        <button type="submit">Enviar</button>
       </form>
     </>
   )
